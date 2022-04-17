8a70391010ddefe7f5b6b92680bddb5f3b86c5a9
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-04-16 21:21:22 -0400

    Proper Layout

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
-import "./App.css";
-import "tachyons";
-import CanvasDraw from "react-canvas-draw";
+import './App.css';
+import 'tachyons';
+import CanvasDraw from 'react-canvas-draw';
 
 const App = () => {
-  const location = "";
   return (
     <div>
-      <div className="ma4">
-        <h1 className="f1 tc lh-title">Handwritten Digits</h1>
-
+      <div>
+        <h1 className="f1 tc lh-title pa3">Handwritten Digits</h1>
+        {/* Left Side */}
         <div className="flex justify-center">
-          <div class="outline w-25 pa3 mr2">
-            <p>Using MNIST dataset to recognize Handwritten Digits</p>
+          <div className="pa3 mr2">
+            <h3>What?</h3>
+            <p>
+              A machine leraning program that can recognize handwritten digits
+              from 0 to 9.
+            </p>
+            <h3>How?</h3>
+            <p>AWS Lambda and Convolutional Neural Networks.</p>
+            <h3>Who?</h3>
+            <p>
+              <a href="https://www.catcodebox.com/">Cat Tran</a>
+            </p>
+            <p>
+              <a href="https://github.com/JonathanCen">Johnathan Cen</a>
+            </p>
           </div>
-          <div class="outline w-25 pa3 mr2">
-            <div>
+
+          {/* Right Side */}
+          <div className="pa3 mr2">
+            <div className="flex justify-center outline">
               <CanvasDraw
-                canvasWidth={200}
-                canvasHeight={200}
+                canvasWidth={300}
+                canvasHeight={300}
                 lazyRadius={1}
-                brushColor={"darkblue"}
+                brushColor={'darkblue'}
               />
+            </div>
+            <div className="flex justify-center">
+              <p>
+                <strong>Prediction:</strong> 5 with 80%
+              </p>
             </div>
           </div>
         </div>
-
         <div className="flex justify-center">
           <a className="f6 link dim ph3 pv2 mb2 dib white bg-black" href="#0">
             Submit
