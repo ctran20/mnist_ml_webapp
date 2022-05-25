9eea2a54aaee3d07307964a4ed45bebecf1ec070
Cat Tran <catntran@Cats-MacBook-Pro.local>
2022-05-25 18:06:07 -0400

    Setting up AWS lambda

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import './App.css';
-import 'tachyons';
-import CanvasDraw from 'react-canvas-draw';
+import "./App.css";
+import "tachyons";
+import axios from "axios";
+import CanvasDraw from "react-canvas-draw";
 
 const App = () => {
+  const test = () => {
+    const api =
+      "https://20zcp67omb.execute-api.us-east-1.amazonaws.com/staging";
+    const data = {
+      key1: "Cat",
+      key2: "N",
+      key3: "Tran",
+    };
+    axios
+      .post(api, data)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <div>
@@ -33,7 +52,7 @@ const App = () => {
                 canvasWidth={300}
                 canvasHeight={300}
                 lazyRadius={1}
-                brushColor={'darkblue'}
+                brushColor={"darkblue"}
               />
             </div>
             <div className="flex justify-center">
@@ -44,7 +63,11 @@ const App = () => {
           </div>
         </div>
         <div className="flex justify-center">
-          <a className="f6 link dim ph3 pv2 mb2 dib white bg-black" href="#0">
+          <a
+            className="f6 link dim ph3 pv2 mb2 dib white bg-black"
+            href="#0"
+            onClick={test()}
+          >
             Submit
           </a>
         </div>
